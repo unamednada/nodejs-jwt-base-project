6a58ba65118b04a1cc1c6d74f15b31b9667bea19
unamednada <dias.gustavo.dias@gmail.com>
2022-02-03 15:19:58 -0300

    Create integration tests and mocks for findall and create. success

--- api/app.js
+++ api/app.js
@@ -2,22 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 
-const validateJWT = require('./auth/validateJWT');
-
-const PORT = process.env.PORT || 8080;
-
-const app = require('./app');
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const apiRoutes = express.Router();
 
-apiRoutes.get('/api/posts', validateJWT, routes.getPosts);
+apiRoutes.get('/api/posts', routes.getPosts);
 apiRoutes.post('/api/users', routes.createUsers);
 apiRoutes.get('/api/users', routes.getUsers);
 apiRoutes.post('/api/login', routes.login);
 
 app.use(apiRoutes);
 
-app.listen(PORT, () => console.log(`Conectado na porta ${PORT}`));
+module.exports = app;
